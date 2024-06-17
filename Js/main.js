4645f565b9146a8e7d7036cdb9bbd8061cf73cae
casey <caseyochieng46@gmail.com>
2024-06-18 00:26:54 +0300

    added pages

--- Js/main.js
+++ Js/main.js
@@ -1,31 +1,29 @@
 console.clear();
 
-const lenis = new Lenis()
+const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
 
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
 
+lenis.on("scroll", ScrollTrigger.update);
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
 
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
-
-gsap.ticker.lagSmoothing(0)
+gsap.ticker.lagSmoothing(0);
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -35,34 +33,34 @@ window.addEventListener("load", () => {
       scrollTrigger: {
         trigger: ".wrapper",
         start: "top top",
+        toggleAction: "play none none reverse",
         end: "+=150%",
         pin: true,
         scrub: true,
-        markers: false
-      }
+        markers: false,
+      },
     })
     .to("img", {
       scale: 2,
       z: 350,
       transformOrigin: "center center",
-      ease: "power1.inOut"
+      ease: "power1.inOut",
     })
     .to(
       ".section.hero",
       {
         scale: 1.1,
         transformOrigin: "center center",
-        ease: "power1.inOut"
+        ease: "power1.inOut",
       },
       "<"
     );
 });
 
-
 // The Title changer
 let title = document.title;
 window.addEventListener("blur", () => {
-  document.title = title = "Thank You 🕺💃";
+  document.title = title = "Thank you for visiting us";
 });
 //Leave it if you dont know how it works
 window.addEventListener("focus", () => {
