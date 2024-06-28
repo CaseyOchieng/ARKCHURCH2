5d01f36908a5b8eb00b04a6cfbf2fbcebafef648
casey <caseyochieng46@gmail.com>
2024-06-28 23:23:10 +0300

    gallery finished

--- Js/gallery.js
+++ Js/gallery.js
@@ -1,26 +1,32 @@
 class App {
   constructor() {
     this.heroImages = [...document.querySelectorAll(".hero__images img")];
+    this.texts = [...document.querySelectorAll(".text__effect")];
+    console.log("Hero Images:", this.heroImages);
+    console.log("Texts:", this.texts);
     this._initialize();
     this._render();
   }
 
   _initialize() {
+    console.log("Initializing...");
     this._setInitialStates();
     this._createLenis();
     this._createIntro();
     this._createHero();
+    this._createTextAnimation();
+    this._createPinnedSection();
   }
 
   _setInitialStates() {
-    gsap.set(".hero__title span, .text__effect p, .fullwidth-image__text", {
+    gsap.set(".hero__title span, .fullwidth-image__text", {
       y: 32,
       opacity: 0,
     });
 
     gsap.set(".hero__images img", {
       opacity: 0,
-      y: gsap.utils.random(100, 50),
+      y: gsap.utils.random(50, 100),
     });
 
     gsap.set(".fullwidth-image img", {
@@ -29,9 +35,11 @@ class App {
   }
 
   _createLenis() {
+    // Ensure Lenis is properly initialized
     this.lenis = new Lenis({
       lerp: 0.1,
     });
+    requestAnimationFrame(this._render.bind(this));
   }
 
   _createIntro() {
@@ -59,32 +67,105 @@ class App {
         0.5
       );
   }
+
   _createHero() {
-    const tl = gsap.timeline();
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
 
     this.heroImages.forEach((image) => {
-      tl.to(image, {
-        ease: "none",
-        yPercent: gsap.utils.random(-100, -50),
+      tl.to(
+        image,
+        {
+          ease: "none",
+          yPercent: gsap.utils.random(-100, -50),
+        },
+        0
+      );
+    });
+  }
+
+  _createTextAnimation() {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".text-block",
+        start: "top center",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+
+    this.texts.forEach((text, index) => {
+      const overlay = text.querySelector(".text__overlay");
+      tl.to(overlay, {
+        scaleX: 0,
       });
     });
   }
 
-  _render(time) {
-    this.lenis.raf(time);
+  _createPinnedSection() {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".fullwidth-image",
+        start: "top top",
+        end: "+=1500",
+        scrub: true,
+        pin: true,
+      },
+    });
 
+    tl.to(".fullwidth-image__overlay", {
+      opacity: 0.4,
+    })
+      .to(
+        ".fullwidth-image",
+        {
+          ease: "expo.out",
+          "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        },
+        0
+      )
+      .to(
+        ".fullwidth-image img",
+        {
+          scale: 1,
+        },
+        0
+      )
+      .to(
+        ".fullwidth-image__text",
+        {
+          y: 0,
+          opacity: 1,
+          ease: "expo.out",
+        },
+        0
+      );
+  }
+
+  _render(time) {
+    if (this.lenis) {
+      this.lenis.raf(time);
+    }
     requestAnimationFrame(this._render.bind(this));
   }
 }
 
-new App();
+document.addEventListener("DOMContentLoaded", () => {
+  new App();
+});
 
 // The Title changer
 let title = document.title;
 window.addEventListener("blur", () => {
-  document.title = title = "Thank you for visiting us";
+  document.title = "Thank you for visiting us";
 });
-//Leave it if you dont know how it works
+
 window.addEventListener("focus", () => {
-  document.title = title = "Ark Youth Church";
+  document.title = "Ark Youth Church";
 });
